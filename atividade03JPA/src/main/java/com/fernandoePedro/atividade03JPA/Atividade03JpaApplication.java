ae656af82f834281e021a67c09624907bc39a90e
SoutANIMAL <fernandosouzads1@gmail.com>
2021-12-11 20:39:11 -0300

    reposicao das duas pastas referentes as atividades

---
+++ atividade03JPA/src/main/java/com/fernandoePedro/atividade03JPA/Atividade03JpaApplication.java
@@ -0,0 +1,13 @@
+package com.fernandoePedro.atividade03JPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Atividade03JpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Atividade03JpaApplication.class, args);
+	}
+
+}
---
+++ atividade03JPA/src/test/java/com/fernandoePedro/atividade03JPA/Atividade03JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernandoePedro.atividade03JPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Atividade03JpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
